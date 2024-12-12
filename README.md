a3cda2ad100c000989be06a89fa8b3709ca1f053
Samuel <samuel.oosterholt20001004@gmail.com>
2024-12-12 05:54:10 +0100

    feat: day11

--- README.md
+++ README.md
@@ -21,7 +21,7 @@
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
-![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
@@ -132,16 +132,16 @@ Both parts: 8161.816ms
 
 ```
 Day 10
-Time part 1: 9.712ms
-Time part 2: 5.987ms
-Both parts: 15.699ms
+Time part 1: 7.818ms
+Time part 2: 7.959ms
+Both parts: 15.777ms
 ```
 
 ```
 Day 11
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 3.794ms
+Time part 2: 89.214ms
+Both parts: 93.008ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 20/50
-Total time: 54655.266ms
+Total stars: 22/50
+Total time: 54748.352ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day11/index.ts
@@ -0,0 +1,96 @@
+import run from "aocrunner";
+
+type Stones = Map<string, number>;
+
+const parseInput = (rawInput: string): Stones => {
+  const input = rawInput.split(" ");
+  const stoneCounts: Stones = new Map();
+
+  for (const stone of input) {
+    stoneCounts.set(stone, (stoneCounts.get(stone) || 0) + 1);
+  }
+
+  return stoneCounts;
+};
+
+
+const stoneMemo: Map<string, string[]> = new Map();
+
+function handle(stone: string): string[] {
+  if (stoneMemo.has(stone)) {
+    return stoneMemo.get(stone)!;
+  }
+
+  const result: string[] = [];
+  if (stone === "0") {
+    result.push("1");
+  } else if (stone.length % 2 === 0) {
+    const left = parseInt(stone.slice(0, stone.length / 2));
+    const right = parseInt(stone.slice(stone.length / 2));
+    result.push(left.toString());
+    result.push(right.toString());
+  } else {
+    result.push((parseInt(stone) * 2024).toString());
+  }
+
+  stoneMemo.set(stone, result);
+  return result;
+}
+
+function blink(stones: Map<string, number>): Stones {
+  const newStones: Stones = new Map();
+
+  for (const [stone, count] of stones) {
+    const blinkedStones = handle(stone);
+
+    for (const blinked of blinkedStones) {
+      newStones.set(
+        blinked,
+        (newStones.get(blinked) || 0) + count
+      );
+    }
+  }
+
+  return newStones;
+}
+
+const solve = (rawInput: string, iterations: number): number => {
+  let stones = parseInput(rawInput);
+
+  for (let i = 0; i < iterations; i++) {
+    stones = blink(stones);
+  }
+
+  let totalStones = 0;
+  let length = 0;
+  console.info(stones.size, stoneMemo.size);
+  for (const stone of stones.keys()) {
+    length += stone.length * stones.get(stone)!;
+  }
+  console.info(length);
+  for (const count of stones.values()) {
+    totalStones += count;
+  }
+
+  return totalStones;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+        125 17
+        `,
+        expected: 55312,
+      },
+    ],
+    solution: (rawInput: string) => solve(rawInput, 25),
+  },
+  part2: {
+    tests: [],
+    solution: (rawInput: string) => solve(rawInput, 75),
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
